8b29013abd0f5dd8122700a7118e11bf58586376
Steve <hoyesa@msn.com>
2019-12-23 15:46:19 -0500

    Refactor to Soup & Alife objects

--- alife.py
+++ alife.py
@@ -1,5 +1,5 @@
 import math
-from random import randrange
+from random import randrange, randint, uniform
 from collections import deque
 
 
@@ -27,6 +27,112 @@ class node():
             print("hello")
         return self.kern.run(args)
 
+class Soup():
+    def __init__(self) -> None:
+        self.kernList = []
+        self.opList = []
+        self.termList = []
+
+    def append(self,kern:kernel) -> None:
+        self.kernList.append(kern)
+        if kern.arity == 0:
+            self.termList.append(kern)
+        else:
+            self.opList.append(kern)
+
+    def getRandKern(self) -> kernel:
+        idx = randint(0,len(self.kernList)-1)
+        return self.kernList[idx]
+
+    def getRandOp(self) -> kernel:
+        idx = randint(0, len(self.opList) - 1)
+        return self.opList[idx]
+
+    def getRandTerm(self) -> kernel:
+        idx = randint(0, len(self.termList) - 1)
+        return self.termList[idx]
+
+class Alife():
+    def __init__(self, soup: Soup, maxLevel:int=2):
+        self.nodeList = []
+        stack = []
+        kern = soup.getRandOp()  # take the first item = this controls the root kernel
+        self.nodeList.append(kern)
+        self.funcString = kern.symbol + '('
+        argIndex = 1
+        while len(stack) > 0 or kern.arity >= argIndex:
+            if len(stack) < maxLevel:  # max level
+                newKern = soup.getRandKern()
+            else:
+                newKern = soup.getRandTerm()
+            self.nodeList.append(newKern)
+            if newKern.arity > 0:
+                stack.append([kern, argIndex])
+                kern = newKern
+                argIndex = 1
+                self.funcString += kern.symbol + '('
+            else:
+                self.funcString = self.funcString + newKern.symbol
+                while argIndex >= kern.arity and len(stack) > 0:
+                    kern, argIndex = stack.pop()
+                    self.funcString = self.funcString + ')'
+                argIndex += 1
+                self.funcString = self.funcString + ','
+        self.funcString = self.funcString.rstrip(',') + ')'
+
+    def spanNodeTree(self) -> float:
+        if self.nodeList[0].arity == 0:
+            return self.nodeList[0].run([])
+        nodeIdx = 1
+        curOp = self.nodeList[0]  # take the first item = this controls the root kernel
+        argIndex = 1
+        opStack = []
+        argStack = []
+
+        while True:
+            # get the next node in the tree
+            curNode = self.nodeList[nodeIdx]
+            nodeIdx += 1
+
+            # calculate the state of the tree spanner
+            state = stateMachine(curNode.arity>0,len(opStack)>0,argIndex==curOp.arity)
+
+            # current node is an operator
+            if 4 <= state <= 7:
+                opStack.append((curOp,argIndex))
+                argIndex = 1
+                curOp = curNode
+
+            # current token is terminal but need more to finish
+            if state in (0,2):
+                argStack.append(curNode.run([]))
+                argIndex += 1
+
+            # current token is finishing terminal in top level operator
+            if state == 1:
+                # push queue; dequeue argList; solve;  RETURN
+                argStack.append(curNode.run([]))
+                tmpArgList = [argStack.pop() for i in range(curOp.arity)]
+                val = curOp.run(tmpArgList[::-1])
+                return val
+
+            # current token is finishing terminal in a lower level operator
+            if state == 3:
+                # push queue; dequeue argList; solve; push solution; pop stack; inc Curbranch
+                argStack.append(curNode.run([]))
+                while True:
+                    tmpArgList = [argStack.pop() for i in range(curOp.arity)]
+                    val = curOp.run(tmpArgList[::-1])
+                    if len(opStack) == 0:
+                        return val
+                    argStack.append(val)
+                    curOp,argIndex = opStack.pop()
+                    if curOp.arity > argIndex:
+                        break
+                argIndex += 1  # TODO bug here?
+
+
+
 def getRandListItem(lst):
     return lst[randrange(len(lst))]
 
@@ -38,112 +144,34 @@ def stateMachine(isOp,isStack,isLastBranch):
     '''
     return int(isLastBranch)+2*int(isStack)+4*int(isOp)
 
-def spanNodeTree(nodeTree):
-    if nodeTree[0].arity == 0:
-        return nodeTree[0].run([])
-    nodeIdx = 1
-    curOp = nodeTree[0]  # take the first item = this controls the root kernel
-    argIndex = 1
-    opStack = []
-    argStack = []
-
-    while True:
-        # get the next node in the tree
-        curNode = nodeTree[nodeIdx]
-        nodeIdx += 1
-
-        # calculate the state of the tree spanner
-        state = stateMachine(curNode.arity>0,len(opStack)>0,argIndex==curOp.arity)
-
-        # current node is an operator
-        if 4 <= state <= 7:
-            opStack.append((curOp,argIndex))
-            argIndex = 1
-            curOp = curNode
-
-        # current token is terminal but need more to finish
-        if state in (0,2):
-            argStack.append(curNode.run([]))
-            argIndex += 1
-
-        # current token is finishing terminal in top level operator
-        if state == 1:
-            # push queue; dequeue argList; solve;  RETURN
-            argStack.append(curNode.run([]))
-            tmpArgList = [argStack.pop() for i in range(curOp.arity)]
-            val = curOp.run(tmpArgList[::-1])
-            return val
-
-        # current token is finishing terminal in a lower level operator
-        if state == 3:
-            # push queue; dequeue argList; solve; push solution; pop stack; inc Curbranch
-            argStack.append(curNode.run([]))
-            while True:
-                tmpArgList = [argStack.pop() for i in range(curOp.arity)]
-                val = curOp.run(tmpArgList[::-1])
-                if len(opStack) == 0:
-                    return val
-                argStack.append(val)
-                curOp,argIndex = opStack.pop()
-                if curOp.arity > argIndex:
-                    break
-            argIndex += 1  # TODO bug here?
-
-def buildNodeTree(kernList,maxLevel=2):
-    val = None
-    nodeList = []
-    stack = []
-    kern = kernList[0]  # take the first item = this controls the root kernel
-    nodeList.append(kern)
-    functString = kern.symbol+'('
-    argIndex = 1
-    while len(stack) > 0 or kern.arity >= argIndex:
-        if len(stack) < maxLevel: # max level
-            newKern = getRandListItem(kernList)
-        else:
-            newKern = getRandListItem(kernList[8:10])
-        nodeList.append(newKern)
-        if newKern.arity > 0:
-            stack.append([kern,argIndex])
-            kern = newKern
-            argIndex = 1
-            functString += kern.symbol + '('
-        else:
-            functString += newKern.symbol
-            while argIndex >= kern.arity and len(stack) > 0:
-                kern,argIndex = stack.pop()
-                functString += ')'
-            argIndex += 1
-            functString += ','
-    functString = functString.rstrip(',') + ')'
-    return nodeList
 
 ## primordial soup of kernel operators
 
-mult = kernel(2,'*', lambda xy:xy[0] * xy[1])
-div  = kernel(2,'/', lambda xy: 0 if xy[1] == 0.0 else xy[0]/xy[1])
-add  = kernel(2,'+', lambda xy:xy[0] + xy[1])
-sub  = kernel(2,'-', lambda xy:xy[0] - xy[1])
-sqr  = kernel(1,'^', lambda xy:xy[0]**2)
-sqrt = kernel(1,'sqrt',lambda xy:math.sqrt(abs(xy[0])))
-ifgt = kernel(4,'if_gt',lambda xy:xy[2] if xy[0] > xy[1] else xy[3])
-ifle = kernel(4,'if_le',lambda xy:xy[2] if xy[0] <= xy[1] else xy[3])
-pi   = kernel(0,'pi',lambda x:3.141592 )
-c4   = kernel(0,'4', lambda x:-4.0)
-exp  = kernel(1,'exp',lambda x: math.exp(34.0) if x[0] > 34.0 else math.exp(x[0]))
-ln   = kernel(1,'ln', lambda x: 0 if x[0] <= 0.0 else math.log(x[0]))
-sin  = kernel(1,'sin',lambda x:math.sin(x[0]))
-cos  = kernel(1,'cos',lambda x:math.cos(x[0]))
-
-kernList = [mult,div,add,sub,sqr,sqrt,ifgt,ifle,pi,c4,exp,ln,sin,cos]
+soup = Soup()
+soup.append(kernel(2,'*', lambda xy:xy[0] * xy[1]))
+soup.append(kernel(2,'/', lambda xy: 0 if xy[1] == 0.0 else xy[0]/xy[1]))
+soup.append(kernel(2,'+', lambda xy:xy[0] + xy[1]))
+soup.append(kernel(2,'-', lambda xy:xy[0] - xy[1]))
+soup.append(kernel(1,'^', lambda xy:xy[0]**2))
+soup.append(kernel(1,'sqrt',lambda xy:math.sqrt(abs(xy[0]))))
+soup.append(kernel(4,'if_gt',lambda xy:xy[2] if xy[0] > xy[1] else xy[3]))
+soup.append(kernel(4,'if_le',lambda xy:xy[2] if xy[0] <= xy[1] else xy[3]))
+soup.append(kernel(0,'pi',lambda x:3.141592 ))
+soup.append(kernel(1,'exp',lambda x: math.exp(34.0) if x[0] > 34.0 else math.exp(x[0])))
+soup.append(kernel(1,'ln', lambda x: 0 if x[0] <= 0.0 else math.log(x[0])))
+soup.append(kernel(1,'sin',lambda x:math.sin(x[0])))
+soup.append(kernel(1,'cos',lambda x:math.cos(x[0])))
+for i in range(10):
+    r = uniform(-4.0,4.0)
+    soup.append(kernel(0,str(r),lambda x: r))
 
 popSize = 500
 maxLevs = 7
 target = 3.0
 
-population = [buildNodeTree(kernList,maxLevs) for i in range(popSize)]
+population = [Alife(soup,maxLevs) for i in range(popSize)]
 
-vals = [spanNodeTree(population[i]) for i in range(len(population))]
+vals = [alife.spanNodeTree() for alife in population]
 
 fitness = [math.fabs(vals[i]-target) for i in range(len(vals))]
 
